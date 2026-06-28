7808f3e10f74ef5c3ee83c5a9659a1b9592d5d60
Smiley98 <smileysofficialcinema@gmail.com>
2026-06-28 13:09:03 -0400

    Added OBB support function

--- game/src/Demo06GJK.cpp
+++ game/src/Demo06GJK.cpp
@@ -27,10 +27,25 @@ namespace demo_gjk
         return position + proj;
     }
 
-    //Vector2 SupportOBB(Vector2 position, Vector2 direction, Vector2 half_extents, Vector2 d)
-    //{
-    //
-    //}
+    Vector2 SupportOBB(Vector2 position, Vector2 direction, Vector2 half_extents, Vector2 d)
+    {
+        // Transform d into OBB's local-space then treat as AABB
+        Vector2 x_axis = direction;
+        Vector2 y_axis = { -direction.y, direction.x };
+        Vector2 d_local =
+        {
+            Vector2DotProduct(d, x_axis),
+            Vector2DotProduct(d, y_axis)
+        };
+
+        Vector2 proj =
+        {
+            d_local.x >= 0.0f ? half_extents.x : -half_extents.x,
+            d_local.y >= 0.0f ? half_extents.y : -half_extents.y
+        };
+
+        return position + x_axis * proj.x + y_axis * proj.y;
+    }
 
     void Load()
     {
@@ -64,11 +79,13 @@ namespace demo_gjk
         Vector2 d = Vector2Normalize(GetMousePosition() - position);
         //Vector2 p = SupportCircle(position, radius, d);
         //Vector2 p = SupportCapsule(position, direction, half_length, radius, d);
-        Vector2 p = SupportAABB(position, half_extents, d);
+        //Vector2 p = SupportAABB(position, half_extents, d);
+        Vector2 p = SupportOBB(position, direction, half_extents, d);
 
         //DrawCircleV(position, radius, RED);
         //DrawCapsule(position, direction, half_length, radius, RED);
-        DrawAABB(position, half_extents, RED);
+        //DrawAABB(position, half_extents, RED);
+        DrawOBB(position, half_extents, direction, RED);
         DrawCircleV(p, 4.0f, BLUE);
 
         // Support point is "poi between shape and plane perpendicular to d"
